e67f685d5c04245c0e84f30f5fdb332c328d8e7f
Christos-Nikolaos Zacharopoulos <christonik@gmail.com>
2022-12-12 18:50:15 +0100

    hyper param optimization launched & minor

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -44,7 +44,8 @@ def test_proper_boundaries(data: pd.DataFrame):
     """
     Test proper longitude and latitude boundaries for properties in and around NYC
     """
-    idx = data['longitude'].between(-74.25, -73.50) & data['latitude'].between(40.5, 41.2)
+    idx = data['longitude'].between(-74.25, -
+                                    73.50) & data['latitude'].between(40.5, 41.2)
 
     assert np.sum(~idx) == 0
 
@@ -59,15 +60,17 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
+
 def test_row_count(data: pd.DataFrame, min_rows: int, max_rows: int):
     """Check that the rows of the dataset is between two given boundaries"""
     assert min_rows < data.shape[0] < max_rows
 
+
 def test_price_range(data: pd.DataFrame, min_price: float, max_price: float):
     """Check that price spans between two given values"""
-    idx = data['price'].between(min_price, max_price)
 
-    assert np.sum(~idx) == 0
+    assert all(data['price'].between(min_price, max_price))
+
 
 def test_nights_range(data: pd.DataFrame, min_nights: float, max_nights: float):
     """Check that minimum_nights spans between two given values"""
